3b59059b5b8962fee2d1e29cf588436e24adf488
Theodore Norvell <theo@mun.ca>
2023-12-08 15:11:00 -0330

    Added Observability to State

--- shared/src/commonMain/kotlin/model/state/CalculatorState.kt
+++ shared/src/commonMain/kotlin/model/state/CalculatorState.kt
@@ -1,4 +1,19 @@
 package model.state
 
-class CalculatorState() {
+//import model.data.Environment
+//import model.data.formula.FloatNumber
+//import model.data.formula.Formula
+
+class CalculatorState : Observable() {
+    //private var stack : MutableList<Formula> = MutableList( 1) { FloatNumber(0.0) }
+    private var stack : MutableList<String> = MutableList( 1) {"hello" }
+
+    //private val env = Environment()
+    fun stack() = stack
+
+    //fun env() = env
+    fun pushSomething() {
+        stack.add(0, "world" )
+        notifyAllOservers()
+    }
 }
