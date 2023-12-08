afa4594124d1ded678ee2e5293ad1906fc549d17
Theodore Norvell <theo@mun.ca>
2023-12-08 16:48:31 -0330

    Add Formula and Environment

---
+++ shared/src/commonMain/kotlin/model/data/Environment.kt
@@ -0,0 +1,45 @@
+package model.data
+
+import model.data.formula.Formula
+
+class Environment {
+    // Invariant.  The environment contains no circularity
+    // in the sense that there is no nonempty, finite sequence of
+    // strings x of length n+1 > 0 such that all are in the key set,
+    // x(1) in map[x(0)].freeVars(), x(2) in map[x(1)].freeVars(), ...,  x(n) in x(n-1).freeVars()
+    // and x(0) in map[x(n)].freeVars().
+    // For example there is no string x such that x in map[x].freeVars().
+
+
+    private val map : MutableMap<String, Formula> = HashMap()
+    fun has( varName : String ) : Boolean {
+        return map.containsKey(varName)
+    }
+
+    fun get( varName : String ) : Formula? {
+        return map[varName]
+    }
+
+    fun canPut( varName : String, formula: Formula) : Boolean {
+        var fringe = formula.freeVars()
+        var found : Boolean
+        while( true ) {
+            found = fringe.contains( varName )
+            if( fringe.isEmpty() || found ) break
+            val listOfSets = fringe.map {
+                when( val f = map[it] ) {
+                    null -> emptySet()
+                    else -> f.freeVars()} }
+            fringe = listOfSets.fold(
+                initial = emptySet(),
+                operation = { acc: Set<String>, strings: Set<String> ->  acc.union(strings)})
+        }
+        return ! found
+    }
+
+    fun put( varName: String, formula: Formula) {
+        check( canPut( varName, formula) )
+        map[varName] = formula
+    }
+
+}
