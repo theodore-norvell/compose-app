55e0f8b749bd211489afa7c222779221ad546fe4
Theodore Norvell <theo@mun.ca>
2023-12-09 16:15:18 -0330

    Uses Material 3 buttons.  Play with colours and arrangements.

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,25 +1,28 @@
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.AbsoluteCutCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import model.state.CalculatorState
@@ -55,9 +58,10 @@ fun App(calculatorState : CalculatorState) {
 @Composable
 private fun mainPage(viewModel : CalculatorViewModel) {
     val uiState : UIState = viewModel.uiState.collectAsState().value
-    Column( Modifier.fillMaxSize(),
+    Column( modifier = Modifier.fillMaxSize().background( Color.DarkGray ),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
+            verticalArrangement = Arrangement.Center,
+        ) {
 
         AnimatedVisibility(uiState.strings.isNotEmpty()) {
             LazyVerticalGrid(
@@ -67,29 +71,23 @@ private fun mainPage(viewModel : CalculatorViewModel) {
                 content = {
                     items(
                         uiState.strings.size,
-                        itemContent = { k -> stackItemView(uiState.strings[k]) })
+                        itemContent = { k -> stackItemView(uiState.strings[k], k==uiState.strings.size-1) })
                 }
             )
         }
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(5.dp),
-            modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp),
+        LazyColumn( // Of Button Rows
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp),
             content = {
                 items(count = uiState.buttons.size,
                     itemContent = { k ->
-                        LazyRow(
-                            horizontalArrangement =  Arrangement.spacedBy(5.dp),
+                        LazyRow( // of Buttons
+                            horizontalArrangement =  Arrangement.SpaceBetween,
                             modifier = Modifier.fillMaxWidth().weight(1.0f),
                             content = {
                                 items(count = uiState.buttons[k].size,
                                     itemContent = { c ->
-                                        Button(
-                                            onClick = { viewModel.click(uiState.buttons[k][c]) }
-                                        ) {
-                                            Text(
-                                                text = uiState.buttons[k][c]
-                                            )
-                                        }
+                                        mkButton(viewModel, uiState.buttons[k][c])
                                     })
                             })
                     })
@@ -98,9 +96,34 @@ private fun mainPage(viewModel : CalculatorViewModel) {
 }
 
 @Composable
-fun stackItemView(str: String) {
+private fun LazyItemScope.mkButton(
+    viewModel: CalculatorViewModel,
+    desc : String
+) {
+
+    val w = 1.0f // when(desc){"enter" -> 2.0f ; else -> 1.0f}
+    ElevatedButton(
+          //modifier = Modifier.background(Color.hsl(241.0f, 0.50f, 0.34f)),
+          colors = ButtonDefaults.buttonColors(
+              containerColor = Color.hsl(220.0f, 0.62f, 0.34f) ),
+        onClick = { viewModel.click(desc) }
+    ) {
+        Text(
+            text = desc,
+            color = Color.White,
+            modifier = Modifier.background(Color.Transparent)
+        )
+    }
+}
+
+@Composable
+fun stackItemView(str: String, isFinal : Boolean) {
     Text(
-        text = str
+        modifier = Modifier.background(Color.hsl(146.0f, 0.11f, 0.65f)),
+        text = str,
+        color = Color.Black,
+        fontSize = 5.0 .em,
+        fontFamily = FontFamily.Monospace,
     )
 }
 expect fun getPlatformName(): String
